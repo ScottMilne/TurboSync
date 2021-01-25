2cadff7d314d3cdaf380fa94f175bbe37772fcf0
Scott Milne <72017529+1905944@users.noreply.github.com>
2021-01-25 13:32:32 +0000

    Add files via upload
    Initial setup

---
+++ public/javascripts/animation.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+
+	
+	////////////////////////// FIRST OF ALL ////////////////////////////////////////////////////////
+
+	const firstOfAll = document.getElementById('firstOfAllAudio');
+	const image2 = document.getElementById('myImage2');
+
+
+	function animateFirstOfAll() {
+		let time = firstOfAll.currentTime;
+		console.log(`Current audio time: ${firstOfAll.currentTime}`);
+		firstOfAllTiming.forEach((item) => {
+			if (item.start < time && time < item.end) image2.src = `/images/${item.value}.png`;
+		})
+		if (!firstOfAll.ended) window.requestAnimationFrame(animateFirstOfAll);
+	}
+
+	function playFirstOfAll() {
+		firstOfAll.play();
+		animateFirstOfAll();
+	}
+
+	document.getElementById('firstOfAllBtn').onclick = playFirstOfAll;
+
+	////////////////////////// WREKT ////////////////////////////////////////////////////////
+
+	const wrekt = document.getElementById('wrektAudio');
+	const image3 = document.getElementById('myImage3');
+
+
+	function animateWrekt() {
+		let time = wrekt.currentTime;
+		console.log(`Current audio time: ${wrekt.currentTime}`);
+		wrektTiming.forEach((item) => {
+			if (item.start < time && time < item.end) image3.src = `/images/${item.value}.png`;
+		})
+		if (!wrekt.ended) window.requestAnimationFrame(animateWrekt);
+	}
+
+	function playWrekt() {
+		wrekt.play();
+		animateWrekt();
+	}
+
+	document.getElementById('wrektBtn').onclick = playWrekt;
+
+	////////////////////////// GOT EEEM ////////////////////////////////////////////////////////
+
+	const gotIm = document.getElementById('gotImAudio');
+	const image4 = document.getElementById('myImage4');
+
+	function animateGotIm() {
+		let time = gotIm.currentTime;
+		console.log("gotim");
+		console.log(`Current audio time: ${gotIm.currentTime}`);
+		gotImTiming.forEach((item) => {
+			if (item.start < time && time < item.end) image4.src = `/images/${item.value}.png`;
+		})
+		if (!gotIm.ended) window.requestAnimationFrame(animateGotIm);
+	}
+
+	function playGotIm() {
+		gotIm.play();
+		animateGotIm();
+	}
+
+	document.getElementById('gotImBtn').onclick = playGotIm;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+	const damage = document.getElementById('damageAudio');
+	const image6 = document.getElementById('myImage6');
+
+	function animateDamage() {
+		let time = damage.currentTime;
+		console.log(`Current audio time: ${damage.currentTime}`);
+		damageTiming.forEach((item) => {
+			if (item.start < time && time < item.end) image6.src = `/images/${item.value}.png`;
+		})
+		if (!damage.ended) window.requestAnimationFrame(animateDamage);
+	}
+
+	function playDamage() {
+		damage.play();
+		animateDamage();
+	}
+
+	document.getElementById('damageBtn').onclick = playDamage;	
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	const hello = document.getElementById('helloAudio');
+	const image7 = document.getElementById('myImage7');
+
+	function animateHello() {
+		let time = hello.currentTime;
+		console.log(`Current audio time: ${hello.currentTime}`);
+		helloTiming.forEach((item) => {
+			if (item.start < time && time < item.end) image7.src = `/images/${item.value}.png`;
+		})
+		if (!hello.ended) window.requestAnimationFrame(animateHello);
+	}
+
+	function playHello() {
+		hello.play();
+		animateHello();
+	}
+
+	document.getElementById('helloBtn').onclick = playHello;	
+	
+	
+	
+})
---
+++ public/javascripts/timing.js
@@ -0,0 +1,52 @@
+const wrektTiming = [
+	{ "start": 0.00, "end": 0.18, "value": "X" },
+	{ "start": 0.18, "end": 0.30, "value": "B" },
+	{ "start": 0.30, "end": 0.37, "value": "C" },
+	{ "start": 0.37, "end": 0.51, "value": "B" },
+	{ "start": 0.51, "end": 0.97, "value": "X" }
+];
+
+const firstOfAllTiming = [
+	{ "start": 0.00, "end": 0.22, "value": "X" },
+	{ "start": 0.22, "end": 0.27, "value": "F" },
+	{ "start": 0.27, "end": 0.60, "value": "C" },
+	{ "start": 0.60, "end": 0.74, "value": "B" },
+	{ "start": 0.74, "end": 0.80, "value": "G" },
+	{ "start": 0.80, "end": 0.92, "value": "E" },
+	{ "start": 0.92, "end": 1.11, "value": "C" },
+	{ "start": 1.11, "end": 1.16, "value": "B" },
+	{ "start": 1.16, "end": 1.20, "value": "G" },
+	{ "start": 1.20, "end": 1.27, "value": "E" },
+	{ "start": 1.27, "end": 1.48, "value": "H" },
+	{ "start": 1.48, "end": 1.77, "value": "X" }
+];
+
+const gotImTiming = [
+	{ "start": 0.00, "end": 0.11, "value": "X" },
+	{ "start": 0.11, "end": 0.30, "value": "C" },
+	{ "start": 0.30, "end": 1.42, "value": "B" },
+	{ "start": 1.42, "end": 1.82, "value": "X" }
+];
+const damageTiming = [
+    { "start": 0.00, "end": 0.11, "value": "X" },
+    { "start": 0.11, "end": 0.23, "value": "D" },
+    { "start": 0.23, "end": 0.30, "value": "B" },
+    { "start": 0.30, "end": 0.44, "value": "E" },
+    { "start": 0.44, "end": 0.58, "value": "F" },
+    { "start": 0.58, "end": 0.65, "value": "D" },
+    { "start": 0.65, "end": 0.79, "value": "B" },
+    { "start": 0.79, "end": 1.14, "value": "C" },
+    { "start": 1.14, "end": 1.23, "value": "A" },
+    { "start": 1.23, "end": 1.44, "value": "B" },
+    { "start": 1.44, "end": 2.08, "value": "X" }
+];
+const helloTiming = [
+    { "start": 0.00, "end": 0.08, "value": "X" },
+    { "start": 0.08, "end": 0.31, "value": "B" },
+    { "start": 0.31, "end": 0.80, "value": "C" },
+    { "start": 0.80, "end": 1.22, "value": "B" },
+    { "start": 1.22, "end": 1.36, "value": "C" },
+    { "start": 1.36, "end": 1.50, "value": "E" },
+    { "start": 1.50, "end": 1.92, "value": "B" },
+    { "start": 1.92, "end": 1.99, "value": "X" }
+  ];
